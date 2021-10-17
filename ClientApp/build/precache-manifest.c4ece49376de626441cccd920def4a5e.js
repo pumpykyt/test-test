3fd1f763c944c4dff1a6402f17325f562ebc3197
Andrew <senseandrey@gmail.com>
2021-10-17 04:54:53 +0300

    init

---
+++ ClientApp/build/precache-manifest.c4ece49376de626441cccd920def4a5e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb83a06762f8947dc0bcd177b6194d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0e64850411ff9c37806",
+    "url": "/static/css/2.f7861fd5.chunk.css"
+  },
+  {
+    "revision": "c50b11e3da4e6e437323",
+    "url": "/static/css/main.40df52f2.chunk.css"
+  },
+  {
+    "revision": "e0e64850411ff9c37806",
+    "url": "/static/js/2.72b08255.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.72b08255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c50b11e3da4e6e437323",
+    "url": "/static/js/main.b55f2925.chunk.js"
+  },
+  {
+    "revision": "ab81e7d6fd57eff4482d",
+    "url": "/static/js/runtime-main.a9395947.js"
+  },
+  {
+    "revision": "842f97f7318517025c67f4281843b301",
+    "url": "/static/media/bg-top.842f97f7.jpg"
+  },
+  {
+    "revision": "6e6a7c567c129e8ff87ee3edb8b14b97",
+    "url": "/static/media/tests.6e6a7c56.svg"
+  }
+]);
